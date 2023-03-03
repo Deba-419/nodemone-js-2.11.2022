10a2a9c9a53511a5552e52b8002cd25fdfb2126c
Debashis Roy <debashis14319@gmail.com>
2023-03-03 23:47:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var studentAge1=30;
+// var studentAge2='10.6';// coercion
+var studentAge2='Deba';
+// console.log(studentAge1);
+var instituteName="Green University of Bangladesh."
+// console.log(instituteName, typeof instituteName, typeof studentAge)
+var isStudent=true
+console.log(typeof isStudent)
+var total=studentAge1+parseFloat(studentAge2);
+console.log(total);
